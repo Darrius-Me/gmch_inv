753112c31d173301410a42864eba21e5977ea0a0
Darrius-Me <63026614+Darrius-Me@users.noreply.github.com>
2020-04-22 16:47:17 +0800

    Optimize data fetching Working data adding **NOT YET WORKING ADDING ITEMS TO PO**

--- po_list/admin.py
+++ po_list/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import PurchaseOrder, Department, Item
 # Register your models here.
+
+admin.site.register(PurchaseOrder)
+admin.site.register(Department)
+admin.site.register(Item)
